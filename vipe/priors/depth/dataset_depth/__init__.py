5af7e339bf4a2b0da64c662727e15e786f55df74
zaid-nasser <znasser@itmo.ru>
2025-09-01 15:33:36 +0300

    Adding dataset config

--- vipe/priors/depth/dataset_depth/__init__.py
+++ vipe/priors/depth/dataset_depth/__init__.py
@@ -4,6 +4,7 @@ from PIL import Image
 import torch
 import torchvision.transforms as T
 import numpy as np
+import cv2
 
 class Datasetdepth(DepthEstimationModel):
     def __init__(self,dataset='replica',datasets_path = '/data/',scene = 'room0'):
@@ -18,30 +19,30 @@ class Datasetdepth(DepthEstimationModel):
     
     def estimate(self, src: DepthEstimationInput) -> DepthEstimationResult:
         depth_img_name = f"depth{src.index:06d}.png"
-        depth_img_path = os.path.join(self.datasets_path, self.dataset, self.scene,depth_img_name)
-        depth_img = Image.open(depth_img_path)
+        depth_img_path = os.path.join(self.datasets_path, self.dataset, self.scene,'results',depth_img_name)
+        depth_img = cv2.imread(depth_img_path, cv2.IMREAD_UNCHANGED)
         to_tensor = T.ToTensor()
         depth_tensor = to_tensor(depth_img)  # shape: (1, H, W), values in [0,1] if uint8
         # If it's 16-bit depth (common for datasets like TUM or KITTI),
         # ToTensor() will still scale to [0,1]. You probably want real depth values.
         depth_array = np.array(depth_img).astype("float32")  # keeps actual depth values
         (h1, w1), (crop_top, crop_bottom, crop_left, crop_right) = self._compute_frame_size_crop(depth_array.shape)
-        depth_resized = np.array(
+        depth_array = np.array(
             Image.fromarray(depth_array).resize((w1, h1), resample=Image.NEAREST),
             dtype=np.float32
         )
 
         # Crop
-        depth_cropped = depth_resized[
+        depth_array = depth_array[
             crop_top : h1 - crop_bottom,
             crop_left : w1 - crop_right
         ]
-        depth_tensor = torch.from_numpy(depth_cropped)  # shape: (H, W)
-        depth_tensor = depth_tensor.unsqueeze(0)
+        depth_array = torch.from_numpy(depth_array)  # shape: (H, W)
+        depth_array = depth_array.unsqueeze(0)
 
         return DepthEstimationResult(
-            metric_depth = depth_tensor,
-            confidence = torch.ones_like(depth_tensor)
+            metric_depth = depth_array,
+            confidence = torch.ones_like(depth_array)
         )   
     
     def _compute_frame_size_crop(self, previous_frame_size: tuple[int, int]):
