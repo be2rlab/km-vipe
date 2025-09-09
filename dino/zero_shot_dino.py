bebe60ed9852aaa55ffeddc19420b2e281521cfb
JaafarMahmoud1 <jaafar.a.mahmoud1@gmail.com>
2025-09-09 10:57:59 +0300

    Small fixes

--- dino/zero_shot_dino.py
+++ dino/zero_shot_dino.py
@@ -1,7 +1,7 @@
 #############
-# DOWNLOAD LINKS
-# backbone: https://dinov3.llamameta.net/dinov3_vitl16/dinov3_vitl16_pretrain_lvd1689m-8aa4cbdd.pth?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoieXRiOTI4d3VwNWt2NXZndmtuNm5wYThwIiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZGlub3YzLmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3NTc1Mzc5NTJ9fX1dfQ__&Signature=cJkGICbE3q-8UnO8-U1GEVcaxHiCg1p6P5ySWzDBojahN19QOHLXkuH1S2mDto7Cw-sHbf3d2GGQhG4B9BZ-3gKbFBbwTzvYwoDZgdyX0xArDpnwXCUs6BCHwXf1yCdPvYK9R7xlDLzIukL43Bujl8JWJwRC3qvM3w5QjDJ0M49FyD8MmSD5YQh81SRqvzGRePLjvlefaHq-W%7El7pvG-DtxMAKE2pEIVpZLY1LxgCS3cbES202DOYerM0twyOFjvdwuDYeZ7jhiz3sovmFCHLzj%7ELpijyhF4tDwyLh7wouOIqP0RZJjmZi41HWLFogRfYIi%7EBDSQw1hljHeyKHD1Hg__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=2300273540405481
-# image+text head: https://dinov3.llamameta.net/dinov3_vitl16/dinov3_vitl16_dinotxt_vision_head_and_text_encoder-a442d8f5.pth?Policy=eyJTdGF0ZW1lbnQiOlt7InVuaXF1ZV9oYXNoIjoieXRiOTI4d3VwNWt2NXZndmtuNm5wYThwIiwiUmVzb3VyY2UiOiJodHRwczpcL1wvZGlub3YzLmxsYW1hbWV0YS5uZXRcLyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE3NTc1Mzc5NTJ9fX1dfQ__&Signature=cJkGICbE3q-8UnO8-U1GEVcaxHiCg1p6P5ySWzDBojahN19QOHLXkuH1S2mDto7Cw-sHbf3d2GGQhG4B9BZ-3gKbFBbwTzvYwoDZgdyX0xArDpnwXCUs6BCHwXf1yCdPvYK9R7xlDLzIukL43Bujl8JWJwRC3qvM3w5QjDJ0M49FyD8MmSD5YQh81SRqvzGRePLjvlefaHq-W%7El7pvG-DtxMAKE2pEIVpZLY1LxgCS3cbES202DOYerM0twyOFjvdwuDYeZ7jhiz3sovmFCHLzj%7ELpijyhF4tDwyLh7wouOIqP0RZJjmZi41HWLFogRfYIi%7EBDSQw1hljHeyKHD1Hg__&Key-Pair-Id=K15QRJLYKIFSLZ&Download-Request-ID=2300273540405481
+# DOWNLOAD LINKS from https://ai.meta.com/resources/models-and-libraries/dinov3-downloads/ 
+# You have to fill a form to get access to the models.
+# This access is temporarily, download all models would be better, otherwise you need to submit the form again.
 #############
 
 import torch
@@ -14,8 +14,8 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 REPO_DIR = "dinov3/"
 
-backbone_weights = f"./weights/dinov3_vitl16_pretrain_lvd1689m-8aa4cbdd.pth"
-weights = f"./weights/dinov3_vitl16_dinotxt_vision_head_and_text_encoder-a442d8f5.pth"
+backbone_weights = f"./weights/dinov3/dinov3_vitl16_pretrain_lvd1689m-8aa4cbdd.pth"
+weights = f"./weights/dinov3/dinov3_vitl16_dinotxt_vision_head_and_text_encoder-a442d8f5.pth"
 
 # DINOv3
 dino_backbone, tokenizer = torch.hub.load(REPO_DIR, 'dinov3_vitl16_dinotxt_tet1280d20h24l', source='local', weights=weights, backbone_weights=backbone_weights)
