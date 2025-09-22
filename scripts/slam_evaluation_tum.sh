b04a78bbee9ef136c34e266652b84c8ccb7dce7b
zaid-nasser <kaartemov@gmail.com>
2025-09-22 19:08:48 +0300

    fixing tum evaluaiton

--- scripts/slam_evaluation_tum.sh
+++ scripts/slam_evaluation_tum.sh
@@ -3,8 +3,10 @@
 export ROOT_DIR=/home/user/km-vipe
 export GT_FOLDER=/data/tum
 export RESULTS_FOLDER=$ROOT_DIR/tum_results
+export CUDA_VISIBLE_DEVICES=1
 export SCENE_NAMES=(
-    rgbd_dataset_freiburg3_sitting_xyz
+    rgbd_dataset_freiburg1_desk
+    # rgbd_dataset_freiburg3_sitting_xyz
     # rgbd_dataset_freiburg3_sitting_halfsphere
     # rgbd_dataset_freiburg3_sitting_static
     # rgbd_dataset_freiburg3_sitting_rpy
@@ -19,20 +21,20 @@ for SCENE_NAME in ${SCENE_NAMES[*]}
 do
     printf "Running scene:   %s\n" "$SCENE_NAME"
 
-    # python3 $ROOT_DIR/scripts/create_video.py \
-    #     --dataset=tum \
-    #     --scene_name=$SCENE_NAME \
-    #     --input_dir=$GT_FOLDER \
-    #     --output_dir=$RESULTS_FOLDER/videos/ \
+    python3 $ROOT_DIR/scripts/create_video.py \
+        --dataset=tum \
+        --scene_name=$SCENE_NAME \
+        --input_dir=$GT_FOLDER \
+        --output_dir=$RESULTS_FOLDER/videos/ \
 
-    # python3 $ROOT_DIR/run.py \
-    #     pipeline=tum \
-    #     streams=raw_mp4_stream \
-    #     pipeline.output.save_artifacts=true \
-    #     streams.base_path=$RESULTS_FOLDER/videos/$SCENE_NAME.mp4 \
-    #     pipeline.output.path=$RESULTS_FOLDER \
-    #     pipeline.slam.dataset.sequence_name=$SCENE_NAME \
-    #     pipeline.slam.keyframe_depth=dataset \
+    python3 $ROOT_DIR/run.py \
+        pipeline=default \
+        streams=raw_mp4_stream \
+        pipeline.output.save_artifacts=true \
+        streams.base_path=$RESULTS_FOLDER/videos/$SCENE_NAME.mp4 \
+        pipeline.output.path=$RESULTS_FOLDER \
+        # pipeline.slam.dataset.sequence_name=$SCENE_NAME \
+        # pipeline.slam.keyframe_depth=unidepth-l \
 
     python $ROOT_DIR/scripts/rmse_evaluation.py \
         --dataset "tum" \
